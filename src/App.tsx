5a7a18acbccdfb421a64e8c4d41eeca257b33be4
ali moeini <68547702+moeiniali@users.noreply.github.com>
2024-08-02 03:23:28 +0330

    compleated project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import './App.css'
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import { TmMainLayout } from './components/ExAllCo';
 import { ConfigProvider } from 'antd';
-import { Router, Route, Routes, RouterProvider, createBrowserRouter } from 'react-router-dom';
-interface RoutesProps {
-  children?: React.ReactNode;
-  location?: Partial<Location> | string;
-}
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 
 const router = createBrowserRouter([
   {
@@ -16,14 +11,7 @@ const router = createBrowserRouter([
 
 
   },
-  {
-    path: "/riple",
-    element: (<>
-      <p>riple</p>
-    </>),
 
-
-  },
 ]);
 
 function App() {
@@ -39,13 +27,11 @@ function App() {
             colorBorder: '#F2B80C', colorBgContainer: '#151515', colorTextPlaceholder: "#fff", colorPrimaryText: '#fff', colorText: '#F2B80C',
             colorPrimary: '#02161D', activeBorderColor: '#F2B80C', controlHeight: 40, hoverBorderColor: '#AA7F00'
           },
-          Button: { colorPrimary: '#F50148', colorPrimaryHover: '#B00033' ,  colorPrimaryActive: '#B00033' }
+          Button: { colorPrimary: '#F50148', colorPrimaryHover: '#B00033', colorPrimaryActive: '#B00033' }
         }
       }}>
 
-        <Provider store={store}>
-          <RouterProvider router={router} />
-        </Provider>
+        <RouterProvider router={router} />
       </ConfigProvider>
     </div>
   );
